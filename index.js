fd405452fadcb33e50f17b07183244a868c6b891
Jason Serafica <seraficaj@gmail.com>
2022-02-22 10:29:27 -0800

    dino index route

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const express = require('express');
-const ejsLayouts = require('express-ejs-layouts');
+const express = require("express");
+const ejsLayouts = require("express-ejs-layouts");
+const fs = require("fs");
 
 const app = express();
 
-// Middleware 
-app.set('view engine', 'ejs');
+// Middleware
+app.set("view engine", "ejs");
 app.use(ejsLayouts);
 
+// Routes
+app.get("/", (req, res) => {
+    res.send(`Dinosaur party!`);
+});
+
+// Dinosaur index route
+app.get("/dinosaurs", (req, res) => {
+    let dinosaurs = fs.readFileSync("./dinosaurs.json");
+    let dinoData = JSON.parse(dinosaurs);
+    res.render("dinosaurs/index", { myDinos: dinoData });
+});
+
 app.listen(4000, () => {
-    console.log(`Listening on Port:4000!`)
+    console.log(`Listening on Port:4000!`);
 });
